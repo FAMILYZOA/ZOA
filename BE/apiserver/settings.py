027b6f9b64d6d6e8634ee822cb7bab189ab09741
asdf134652 <asdf134652@gmail.com>
2022-10-24 13:57:36 +0900

    feat: 🎸 [BE-accounts] Add accounts app
    accounts 앱 만들기

--- BE/apiserver/settings.py
+++ BE/apiserver/settings.py
@@ -38,12 +38,18 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    #myapp 
+    'accounts',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd-party
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
