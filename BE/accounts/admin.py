999eb42a38fd64a31559e5eaf2812e6c605941e2
asdf134652 <asdf134652@gmail.com>
2022-10-24 14:52:37 +0900

    feat: 🎸 [BE-accounts] Add User Model
    - User model 생성
    - media 폴더 등록
    - admin page 세팅
    - pillow library 추가

--- BE/accounts/admin.py
+++ BE/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from accounts.models import User
+
+# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+# Register your models here.
+admin.site.register(User,UserAdmin)
--- BE/accounts/models.py
+++ BE/accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractUser)
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+from .manager import CustomUserManager
 
 # Create your models here.
+class User(AbstractUser) :
+
+#------------사용하지 않을 기본 상속 필드 ------------------------------
+    username = None
+    first_name = None
+    last_name = None
+    last_login = None
+    groups = None
+    date_joined = None
+    email = None
+
+#-------------사용할 필드 ---------------------------------------
+    id = models.AutoField(primary_key=True)
+    phone = models.CharField(max_length=11, unique=True)
+    name = models.CharField(max_length=8)
+    birth = models.CharField(max_length=8)
+    image = models.ImageField(upload_to='user/profile/',
+                default='user/profile/profile_default1.png',
+                null=True,blank=True)
+    
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
+
+    def __str__(self) :
+        return self.name
