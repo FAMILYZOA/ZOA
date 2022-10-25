eb8822ed9b59c3bac8e4289b8f453ce1ce70eb28
kiki <unhhyyee66@gmail.com>
2022-10-25 13:56:45 +0900

    feat: 🎸 complete header component
    헤더 컴포넌트 완성

--- frontend/src/pages/auth/prelogin.tsx
+++ frontend/src/pages/auth/prelogin.tsx
@@ -4,19 +4,17 @@ import Header from '../../components/header';
 
 export interface IAppProps {
 }
+;
 
-const Box = styled.div`
-  background: rgb(255, 120, 127);
-`;
-
+const Prelogin = styled.div`
+`
 
 export default class App extends React.Component<IAppProps> {
   public render() {
     return (
-      <Box>
-        <p>혹시!?!!</p>
-        <Header></Header>
-      </Box>
+      <Prelogin>
+        <Header label='프리로그인' back={true} ></Header>
+      </Prelogin>
     );
   }
 }
