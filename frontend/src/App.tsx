e8e9c6f772aab761d2cf003993b87109026c4896
kiki <unhhyyee66@gmail.com>
2022-10-25 17:53:24 +0900

    feat: 🎸 kakao login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import Login from "./pages/auth/login";
 import Prelogin from "./pages/auth/prelogin";
 import Signup from "./pages/auth/signup";
-import Kakao from "./pages/auth/kakaoSignUp"
+import KS from "./pages/auth/kakao/kakaoSignUp"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -16,7 +16,7 @@ function App() {
           <Route path="/" element={<Prelogin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/kao" element={<Kakao />} />
+          <Route path="/kao" element={<KS />} />
         </Routes>
       </div>
     </Router>
