6057776f5bc12621a48e4bc8a321117e4f02556b
asdf134652 <asdf134652@gmail.com>
2022-10-25 12:57:51 +0900

    feat: 🎸 [be-family] create API

--- BE/families/views.py
+++ BE/families/views.py
@@ -1,1 +1,25 @@
 from django.shortcuts import render
+from rest_framework.generics import (
+    CreateAPIView,GenericAPIView,mixins)
+
+from families.models import Family
+from .serializers import FamilySerializer
+from rest_framework.response import Response
+
+
+class FamilyCreateAPIView(CreateAPIView) :
+    serializer_class = FamilySerializer
+    queryset = Family.objects.all()
+    def post(self, request, *args, **kwargs):
+        print(request.user)
+        return self.create(request, *args, **kwargs)
+
+
+    
+
+
+
+
+
+
+
