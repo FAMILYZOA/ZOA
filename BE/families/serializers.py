4108da62750ef9c9d1c8d495c5cc57f27e5ca249
asdf134652 <asdf134652@gmail.com>
2022-10-27 17:54:24 +0900

    feat: 🎸 [BE-family] Family interaction API
    - 가족 생성 권한 부여
    - 가족 이름 설정 table 설계

--- BE/families/serializers.py
+++ BE/families/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from accounts.models import User
 from accounts.serializers import UserSerializer
-from families.models import Family
+from families.models import Family, FamilyInteractionName
 
 class FamilySerializer(serializers.ModelSerializer) :
 
@@ -9,8 +9,30 @@ class FamilySerializer(serializers.ModelSerializer) :
         model = Family
         fields= '__all__'
 
+
+class FamilyNameSetSerializer(serializers.ModelSerializer) :
+
+    class Meta :
+        model = FamilyInteractionName
+        fields = '__all__'
+        read_only_fields = ('family','to_user','from_user')
+
+    def create(self, validated_data):
+        print(validated_data)
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
+
+
 class FamilyRetriveSerializer(serializers.ModelSerializer) :
     users = UserSerializer(many=True,read_only=True)
+    users_name = FamilyNameSetSerializer(many=True,read_only=True)
     class Meta :
         model = Family 
-        fields=('id','name','users',)
+        fields=('id','name','users','users_name',)
+
+class FamilyUpdateSerializer(serializers.ModelSerializer) :
+
+    class Meta :
+        model = Family 
+        fields = ('id','name')
--- BE/families/views.py
+++ BE/families/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404, render
 from rest_framework.generics import (
-    CreateAPIView,RetrieveUpdateDestroyAPIView,mixins)
+    CreateAPIView,RetrieveUpdateDestroyAPIView,UpdateAPIView)
 from accounts.models import User
 from django.http import JsonResponse
-from families.models import Family
-from .serializers import FamilyRetriveSerializer, FamilySerializer
+from families.models import Family, FamilyInteractionName
+from .serializers import FamilyNameSetSerializer, FamilyRetriveSerializer, FamilySerializer, FamilyUpdateSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -16,11 +16,13 @@ class FamilyCreateAPIView(CreateAPIView) :
     queryset = Family.objects.all()
 
     def create(self, request, *args, **kwargs):
+        if request.user.family_id :
+            return Response({'user님은 이미 가족에 가입되어 있습니다.'},status=status.HTTP_400_BAD_REQUEST)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        family = Family.objects.get(id=serializer.data['id'])
+        family = get_object_or_404(Family,id=serializer.data['id'])
         family.users.add(request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
@@ -31,10 +33,28 @@ class FamilyAPIView(RetrieveUpdateDestroyAPIView) :
     lookup_field = 'id'
     def retrieve(self, request, id,*args, **kwargs):
         # instance = self.get_object()
-        instance = Family.objects.get(id=id)
+        instance = get_object_or_404(Family,id=id)
         print(instance.users.all())
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        if request.user.family_id != self.get_object():
+            return Response({'변경 권한이 없습니다.'},status=status.HTTP_403_FORBIDDEN)
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = FamilyUpdateSerializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+
 
 @swagger_auto_schema(
         method='post',
@@ -71,3 +91,40 @@ def UserJoinFamily(request,family_id) :
                 'success' : f'{user.name}님이 {family.name}에 가입되었습니다.'
             }
             return JsonResponse(context,status=status.HTTP_200_OK)
+
+
+class FamilyNameSetAPIView(CreateAPIView,UpdateAPIView) :
+
+    serializer_class = FamilyNameSetSerializer
+    queryset = User.objects.all()
+    lookup_field = 'id'
+
+    def create(self, request, *args, **kwargs):
+        to_user = self.get_object()
+        if FamilyInteractionName.objects.filter(to_user=to_user,from_user=self.request.user).exists() :
+            return Response({f'이미 {to_user}님의 이름을 설정했습니다. 이름 수정만 가능합니다.'},status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    def perform_create(self, serializer):
+        serializer.save(to_user=self.get_object(),from_user=self.request.user,family=self.request.user.family_id)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = get_object_or_404(FamilyInteractionName,to_user=self.get_object(),from_user=self.request.user)
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
