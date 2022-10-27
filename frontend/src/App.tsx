336a2605b009416e6a237230f64ee7e171c111b9
kiki <unhhyyee66@gmail.com>
2022-10-27 16:46:35 +0900

    feat: 🎸 create checklist reading page
    체크리스트 목록 페이지 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Login from "./pages/auth/login";
 import Prelogin from "./pages/auth/prelogin";
 import Signup from "./pages/auth/signup";
+import CreateChecklist from "./pages/checklist/CreateChecklist";
+import ReadChecklist from "./pages/checklist/ReadChecklist";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -13,7 +15,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Prelogin />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} /> 
+          <Route path="/createChecklist" element={<CreateChecklist />} /> 
+          <Route path="/Checklist" element={<ReadChecklist />} /> 
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/pages/checklist/ReadChecklist.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../../components/header";
+
+
+function ReadChecklist(){
+    return (
+      <div>
+        <Header label="할 일 목록" back="true"></Header>
+      </div>
+    );
+}
+
+export default ReadChecklist;
