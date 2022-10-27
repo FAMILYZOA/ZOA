6fe005dc04550ebe9a2d5413aaf9da4002ef93e2
kiki <unhhyyee66@gmail.com>
2022-10-27 16:35:10 +0900

    fix: 🐛 merge error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Login from "./pages/auth/login";
 import Prelogin from "./pages/auth/prelogin";
 import Signup from "./pages/auth/signup";
-import Kakao from "./pages/auth/kakaoSignUp"
 import CreateChecklist from "./pages/checklist/CreateChecklist";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -16,7 +15,6 @@ function App() {
           <Route path="/" element={<Prelogin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/kao" element={<Kakao />} />
           <Route path="/createChecklist" element={<CreateChecklist />} />
         </Routes>
       </div>
