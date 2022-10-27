b731af4f3c28c38a470231ec7fb26063e911e745
hohyun <hohyun323@naver.com>
2022-10-27 21:17:15 +0900

    feat: 🎸 [BE-checklist-model]
    Checklist Model create

--- BE/main/serializers.py
+++ BE/main/serializers.py
@@ -1,16 +1,1 @@
-from rest_framework import serializers
-from main.models import Checklist
-from accounts.models import User
-
-
-class  ChecklistSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model = Checklist
-        fields= '__all__'
-
-
-class  ChecklistDetailSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model = Checklist
-        fields= '__all__'
 
--- BE/main/urls.py
+++ BE/main/urls.py
@@ -1,20 +1,8 @@
 from django.urls import path
-from .views import (
-    DdayAPIView,
-    ChecklistCreateAPIView, 
-    ChecklistSearchAPIView, 
-    ChecklistUpdateDeleteAPIView
-)
+
 
 app_name = 'main'
 
 urlpatterns = [
-    # D-day
-    path('Dday', DdayAPIView.as_view(), name="Dday"),
-    # 체크리스트 작성
-    path('checklist', ChecklistCreateAPIView.as_view(), name="checklistcreate"),
-    # 체크리스트 조회
-    path('checklist/{toUserPK}', ChecklistSearchAPIView.as_view(), name="checklistsearch"),
-    # 체크리스트 수정 및 삭제
-    path('checklist/{checklistPK}', ChecklistUpdateDeleteAPIView.as_view(), name="checklistupdatedelete"),
+
 ]
