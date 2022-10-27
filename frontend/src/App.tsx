a1401c24905ea1dca2229d548a120a8bb0cf1ea9
kiki <unhhyyee66@gmail.com>
2022-10-27 16:29:44 +0900

    fix: 🐛 git error fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import logo from "./logo.svg";
 import Login from "./pages/auth/login";
 import Prelogin from "./pages/auth/prelogin";
 import Signup from "./pages/auth/signup";
-import KS from "./pages/auth/kakao/kakaoSignUp"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -16,7 +15,6 @@ function App() {
           <Route path="/" element={<Prelogin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/kao" element={<KS />} />
         </Routes>
       </div>
     </Router>
