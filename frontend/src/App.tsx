db37354f91fe114f7074a31a14cd4365a5222f77
SSAFY <noin1996@naver.com>
2022-10-28 12:43:12 +0900

    fix: 🐛 [FE-auth] modified CSS of login.tsx, signup.tsx
    login.tsx와 signup.tsx에서 css가 잘못 적용되어 보이는 문제가 발생, 그로인해 수정하여 올립니다.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,9 @@ import Prelogin from "./pages/auth/prelogin";
 import Signup from "./pages/auth/signup";
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { FamilyManage } from './pages/family'
+import ScrumCreate from './pages/scrum/scrumCreate';
 import FamilyCreate from './pages/family/FamilyCreate';
 import FamilyNameEdit from './pages/family/FamilyNameEdit';
-import "./pages/page";
 
 import './App.css';
 
@@ -18,6 +18,7 @@ function App() {
           <Route path="/family/manage/:id" element={ <FamilyManage /> }></Route>
           <Route path="/family/create" element={ <FamilyCreate /> }></Route>
           <Route path="/family/edit" element={ <FamilyNameEdit /> }></Route>
+          <Route path="/scrum/create" element={ <ScrumCreate />}></Route>
           <Route path="/" element={<Prelogin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
