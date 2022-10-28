6b76c2d3a9d51bf928f9bc841eda4449478da50f
mindot7 <codemj11@gmail.com>
2022-10-28 17:45:40 +0900

    feat: App에 주소 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,10 @@ import Prelogin from "./pages/auth/prelogin";
 import Signup from "./pages/auth/signup";
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { FamilyManage } from './pages/family'
-import ScrumCreate from './pages/scrum/scrumCreate';
+// import ScrumCreate from './pages/scrum/scrumCreate';
 import FamilyCreate from './pages/family/FamilyCreate';
 import FamilyNameEdit from './pages/family/FamilyNameEdit';
+import ScrumHome from './pages/scrum/ScrumHome';
 
 import './App.css';
 
@@ -18,10 +19,11 @@ function App() {
           <Route path="/family/manage/:id" element={ <FamilyManage /> }></Route>
           <Route path="/family/create" element={ <FamilyCreate /> }></Route>
           <Route path="/family/edit" element={ <FamilyNameEdit /> }></Route>
-          <Route path="/scrum/create" element={ <ScrumCreate />}></Route>
+          {/* <Route path="/scrum/create" element={ <ScrumCreate />}></Route> */}
           <Route path="/" element={<Prelogin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/scrum" element={<ScrumHome />} />
         </Routes>
       </BrowserRouter>
     </div>
