968786dd0a54dd4b3f924ac33f695bb2b165a265
kiki <unhhyyee66@gmail.com>
2022-10-29 19:58:20 +0900

    feat: 🎸 connecting kakao login api
    카카오 로그인/회원가입 분기처리 api연결

--- frontend/src/pages/auth/kakao/Test.js
+++ frontend/src/pages/auth/kakao/Test.js
@@ -1,4 +1,3 @@
-import { escapeRegExp } from "lodash";
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
@@ -11,6 +10,12 @@ function Test() {
     const params = new URL(document.location).searchParams;
     const kakao_code = params.get('code');
     const navigate = useNavigate();
+    const [token, setToken] = useState('')
+    const [info, setInfo] = useState({
+        id : '',
+        name : '',
+        profile : '',
+    })
     
     const getToken =() => {
         fetch("https://kauth.kakao.com/oauth/token", {
@@ -24,6 +29,7 @@ function Test() {
           .then((res) => res.json())
           .then((data) => {
             if (data.access_token) {
+                setToken(data.access_token);
               localStorage.setItem("token", data.access_token);
             } else {
               console.log("실패");
@@ -38,15 +44,31 @@ function Test() {
                   "Authorization" : `Bearer ${localStorage.token}`
               },
             })
-            .then(
-                res => console.log(res)
+            .then((res) => {
+                const id = String(res.data.id)
+                const data = new FormData();
+                data.append("kakao_id", id)
+                axios({
+                  method: "POST",
+                  url: `https://k7b103.p.ssafy.io/api/v1/accounts/kakao/`,
+                  data: data,
+                }).then((result) => {
+                  console.log('로그인 완료');
+                }).catch((err) => {
+                    if (err.response.status === 401) {
+                        setInfo({
+                          id: res.data.id,
+                          name: res.data.kakao_account.profile.nickname,
+                          profile:
+                            res.data.kakao_account.profile.profile_image_url,
+                        });
+                    } else{
+                        console.log('예상치 못한 에러군,,,');
+                    }
+                })
+            }
             )
           )
-          
-        //   .then((data)=> {
-        //     console.log(data);
-        //   })
-          
     }
 
     useEffect(() => {
@@ -54,11 +76,16 @@ function Test() {
         getToken();
     }, [])
 
+    useEffect(()=>{
+        console.log(info);
+    },[info])
+
 
     return (
       <div>
         왜!!!
         <p>{kakao_code}</p>
+        <p>{token}</p>
       </div>
     );
 }
--- frontend/src/pages/auth/prelogin.js
+++ frontend/src/pages/auth/prelogin.js
@@ -6,6 +6,8 @@ import {RiKakaoTalkFill} from 'react-icons/ri'
 import { useNavigate } from 'react-router-dom';
 import { KakaoLogin } from 'react-kakao-login';
 
+/*global Kakao*/
+
 
 const Prelogin = styled.div`
     display: grid;
@@ -70,66 +72,19 @@ const TextBox = styled.div`
 
 
 function Btns(){
-    const [info, setInfo] = useState({
-      name: '',
-      birth:'',
-      phone:'',
-      profileImg:'',
-    });
-
-
     const navigate = useNavigate();
     const NavZoa = () => {
         navigate(`/login`)
     }
 
     const clickKakaoLogin = () => {
-      //일단 userAgent 를 가져와서, 네이티브 앱인지 알아오자.
-      if (/MY_HYBRID_APP/i.test(navigator.userAgent)) {
-        console.log("하이브리드앱에서 호출하였다.");
-        window.location.href = "/MY_KAKAO_LOGIN_URL";
-        return;
-      }
-
-      //일반적인 브라우저 환경이라면, 카카오 Javascript SDK 를 이용한 카카오로그인을 시도한다.
-      const Kakao = window.Kakao;
-      const redirectUri = "http://localhost:3000/";
-      
-
-      Kakao.Auth.login({
-        // redirectUri,
-        // success는 인증 정보를 응답(response)으로 받는다.
-        success: function (response) {
-          //카카오 SDK에 사용자 토큰을 설정한다.
-          window.Kakao.Auth.setAccessToken(response.access_token);
-          console.log(`is set?: ${window.Kakao.Auth.getAccessToken()}`);
-
-          var ACCESS_TOKEN = window.Kakao.Auth.getAccessToken();
-
-          window.Kakao.API.request({
-            url: "/v2/user/me",
-            success: function ({ kakao_account }) {
-              //어떤 정보 넘어오는지 확인
-              console.log(kakao_account);
-              const { email, profile } = kakao_account;
-              console.log(profile.nickname);
-              setInfo({
-                ...info, name: profile.nickname
-              })
-              console.log('state', info);
-            },
-            fail: function (error) {
-              console.log(error);
-            },
-          });
-        },
-        fail: function (error) {
-          console.log(error);
-        },
-      });
+      Kakao.Auth.authorize({
+        redirectUri: "http://localhost:3000/",
+      })
+      .then(
+        
+      )
     };
-
-
 
 
     return (
