5c0d25d35f0aec6000574df45d9e8488e67d06e9
박재현 <erict4f@gmail.com>
2022-10-30 18:30:53 +0900

    feat: [FE-settings] 설정 화면 구성 완료

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,9 @@ import Login from "./pages/auth/login";
 import Prelogin from "./pages/auth/prelogin";
 import Signup from "./pages/auth/signup";
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import { FamilyManage } from './pages/family'
-import ScrumCreate from './pages/scrum/scrumCreate';
+import { FamilyManage } from './pages/family';
+import { Settings } from './pages/settings';
+// import ScrumCreate from './pages/scrum/scrumCreate';
 import FamilyCreate from './pages/family/FamilyCreate';
 import FamilyNameEdit from './pages/family/FamilyNameEdit';
 
@@ -18,10 +19,11 @@ function App() {
           <Route path="/family/manage/:id" element={ <FamilyManage /> }></Route>
           <Route path="/family/create" element={ <FamilyCreate /> }></Route>
           <Route path="/family/edit" element={ <FamilyNameEdit /> }></Route>
-          <Route path="/scrum/create" element={ <ScrumCreate />}></Route>
+          {/* <Route path="/scrum/create" element={ <ScrumCreate />}></Route> */}
           <Route path="/" element={<Prelogin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </BrowserRouter>
     </div>
