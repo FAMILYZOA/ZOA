4efea4a58393a9527ad6131144172d247df6c47f
unhhyyeexx <unhhyyee66@gmail.com>
2022-10-31 20:53:26 +0900

    refactor: 💡 main page path setting
    메인 페이지 카카오 로그인 로직 연결 후 test.js 함수 메인페이지로 이동, 메인페이지 url '/'로 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,18 @@
 import React, { useEffect } from "react";
-import Main from './pages/main/main';
+import Main from "./pages/main/main";
 import Login from "./pages/auth/login";
 import Prelogin from "./pages/auth/prelogin";
 import Signup from "./pages/auth/signup";
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import { FamilyManage } from './pages/family'
-import ScrumCreate from './pages/scrum/scrumCreate';
-import FamilyCreate from './pages/family/FamilyCreate';
-import FamilyNameEdit from './pages/family/FamilyNameEdit';
-import Test from "./pages/auth/kakao/Test";
-import KakaoSignup from './pages/auth/kakao/kakaoSignUp';
-import './App.css';
-import Resister from './pages/auth/Resister';
-import NewLogin from "./pages/auth/kakao/Login"
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { FamilyManage } from "./pages/family";
+import ScrumCreate from "./pages/scrum/scrumCreate";
+import FamilyCreate from "./pages/family/FamilyCreate";
+import FamilyNameEdit from "./pages/family/FamilyNameEdit";
+import KakaoSignup from "./pages/auth/kakao/kakaoSignUp";
+import "./App.css";
+import Resister from "./pages/auth/Resister";
+import NewLogin from "./pages/auth/kakao/Login";
 import { ReadChecklist, CreateChecklist } from "./pages/checklist";
-
-
 
 import Navbar from "./components/Navbar";
 
@@ -133,14 +130,13 @@ function App() {
           <Route path="/intro" element={<Prelogin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/main" element={<Main />} />
+          <Route path="/" element={<Main />} />
           <Route path="/checklist/create" element={<CreateChecklist />} />
           <Route path="/checklist" element={<ReadChecklist />} />
-          <Route path="/" element={<Test />} />
           <Route path="/kakaoSignup" element={<KakaoSignup />} />
           <Route path="/register" element={<Resister />} />
           <Route path="/newLogin" element={<NewLogin />} />
-          <Route path="/register" element={<Resister />} /> 
+          <Route path="/register" element={<Resister />} />
         </Routes>
         <Navbar></Navbar>
       </BrowserRouter>
--- frontend/src/pages/main/main.js
+++ frontend/src/pages/main/main.js
@@ -5,15 +5,90 @@ import Emojis from "../../components/main/Emoji";
 import Announcement from "../../components/main/Announcement";
 import { useAppSelector } from "../../app/hooks";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
+function KakaoLogin() {
+  const params = new URL(document.location).searchParams;
+  const kakao_code = params.get("code");
+  const navigate = useNavigate();
+  const [info, setInfo] = useState({
+    id: "",
+    name: "",
+    profile: "",
+  });
 
+  const getToken = () => {
+    fetch("https://kauth.kakao.com/oauth/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `grant_type=authorization_code&client_id=${"931a81a6fdb9751f2858ca6f2f46b377"}&redirect_uri=${"http://localhost:3000/"}&code=${kakao_code}`,
+      //prompt={none}(?) 추가하면 자동로그인 된다 함
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.access_token) {
+          localStorage.setItem("token", data.access_token);
+        } else {
+          navigate("/intro");
+        }
+        axios({
+          method: "GET",
+          url: `https://kapi.kakao.com/v2/user/me`,
+          headers: {
+            Authorization: `Bearer ${localStorage.token}`,
+          },
+        }).then((res) => {
+          const id = String(res.data.id);
+          const data = new FormData();
+          data.append("kakao_id", id);
+          axios({
+            method: "POST",
+            url: `https://k7b103.p.ssafy.io/api/v1/accounts/kakao/`,
+            data: data,
+          })
+            .then((result) => {
+              if (result.status === 200) {
+                localStorage.setItem("access_token", result.data.token.access);
+                localStorage.setItem(
+                  "refresh_token",
+                  result.data.token.refresh
+                );
+              }
+            })
+            .catch((err) => {
+              if (err.response.status === 401) {
+                setInfo({
+                  id: res.data.id,
+                  name: res.data.kakao_account.profile.nickname,
+                  profile: res.data.kakao_account.profile.profile_image_url,
+                });
+              } else {
+                console.log("예상치 못한 에러군,,,");
+              }
+            });
+        });
+      });
+  };
+
+  useEffect(() => {
+    if (!document.location.search) return;
+    getToken();
+  }, []);
+
+  useEffect(() => {
+    if (info.id !== "") {
+      navigate("/kakaoSignup", { state: info });
+    }
+  }, [info]);
+}
 
 function Main() {
   const token = useAppSelector((state) => state.token.access);
   const family = useAppSelector((state) => state.family.id);
   const [scrum, setScrum] = useState([]);
 
-  
   useEffect(() => {
     axios({
       method: "GET",
@@ -26,11 +101,13 @@ function Main() {
     });
   }, [family]);
 
-  return <div>
-    <Header></Header>
-    <Emojis scrum = {scrum}></Emojis>
-    <Announcement scrum = {scrum}></Announcement>
-  </div>;
+  return (
+    <div>
+      <Header></Header>
+      <Emojis scrum={scrum}></Emojis>
+      <Announcement scrum={scrum}></Announcement>
+    </div>
+  );
 }
 
 export default Main;
