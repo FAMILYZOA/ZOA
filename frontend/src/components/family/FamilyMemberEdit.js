fd24192e0189016930334426b8de9cf6f066cf36
mindot7 <codemj11@gmail.com>
2022-11-02 02:35:03 +0900

    feat: familiy_edit api done

--- frontend/src/components/family/FamilyMemberEdit.js
+++ frontend/src/components/family/FamilyMemberEdit.js
@@ -1,15 +1,20 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import FamilyMemberList from "./FamilyMemberList";
+import { useAppSelector } from "../../app/hooks";
 
 const FamilyMemberEdit = () => {
+
+  const token = useAppSelector((state) => state.token.access);
+  const familyId = useAppSelector((state) => state.family.id);
+
 
   useEffect(() => {
     axios({
       method: "get",
-      url: 'https://k7b103.p.ssafy.io/api/v1/family/5/',
+      url: `https://k7b103.p.ssafy.io/api/v1/family/${familyId}/`,
       headers: {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY3MTk2MjAxLCJpYXQiOjE2NjcxMDk4MDEsImp0aSI6ImZhYmY4OTA4YmI0NjRkZGY4YWMxZTFmYTM2ZjAwYzg4IiwidXNlcl9pZCI6MjJ9.10bBY_OlYzH9K4Ct35oelsEBCZrmlnawsiBsqDVNFNU'
+        Authorization: `Bearer ${token}`
       },
     })
     .then((res) => {
