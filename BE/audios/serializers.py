9ec60021e9439f21877fdec2bb04507ad0fa5c83
asdf134652 <asdf134652@gmail.com>
2022-11-01 16:24:47 +0900

    refactor: 💡 [be-audio] refactoring serializer  #968 리스트에 field 가 string으로 들어오는 문제 해결중

--- BE/audios/serializers.py
+++ BE/audios/serializers.py
@@ -3,8 +3,12 @@ from rest_framework import serializers
 from .models import Audio
 from django.db import models
 from django.core.validators import int_list_validator
+
+class IntegerListField(serializers.ListField) :
+    child = serializers.IntegerField()
+
 class AudioSerializer(serializers.ModelSerializer):
-    to_user_id = serializers.ListField(child = serializers.IntegerField())
+    to_user_id = IntegerListField(write_only=True)
     audio = serializers.FileField(
         validators=[FileExtensionValidator(allowed_extensions=['m4a', 'mp3'])])
 
@@ -12,47 +16,3 @@ class AudioSerializer(serializers.ModelSerializer):
         model = Audio
         read_only_fields = ('id','from_user_id',)
         fields = '__all__'
-
-    # def create(self, validated_data):
-    #     raise_errors_on_nested_writes('create', self, validated_data)
-
-    #     ModelClass = self.Meta.model
-
-    #     # Remove many-to-many relationships from validated_data.
-    #     # They are not valid arguments to the default `.create()` method,
-    #     # as they require that the instance has already been saved.
-    #     info = model_meta.get_field_info(ModelClass)
-    #     many_to_many = {}
-    #     for field_name, relation_info in info.relations.items():
-    #         if relation_info.to_many and (field_name in validated_data):
-    #             many_to_many[field_name] = validated_data.pop(field_name)
-
-    #     try:
-    #         instance = ModelClass._default_manager.create(**validated_data)
-    #     except TypeError:
-    #         tb = traceback.format_exc()
-    #         msg = (
-    #             'Got a `TypeError` when calling `%s.%s.create()`. '
-    #             'This may be because you have a writable field on the '
-    #             'serializer class that is not a valid argument to '
-    #             '`%s.%s.create()`. You may need to make the field '
-    #             'read-only, or override the %s.create() method to handle '
-    #             'this correctly.\nOriginal exception was:\n %s' %
-    #             (
-    #                 ModelClass.__name__,
-    #                 ModelClass._default_manager.name,
-    #                 ModelClass.__name__,
-    #                 ModelClass._default_manager.name,
-    #                 self.__class__.__name__,
-    #                 tb
-    #             )
-    #         )
-    #         raise TypeError(msg)
-
-    #     # Save many-to-many relationships after the instance is created.
-    #     if many_to_many:
-    #         for field_name, value in many_to_many.items():
-    #             field = getattr(instance, field_name)
-    #             field.set(value)
-
-    #     return instance
--- BE/audios/views.py
+++ BE/audios/views.py
@@ -15,11 +15,12 @@ class AudioSaveAPIView(CreateAPIView):
     def post(self, request, *args, **kwargs):
         file_obj = request.data
         print(request.data)
+        print(type(request.data['to_user_id']))
+        print(len(request.data['to_user_id']))
         serializer = AudioSerializer(data=file_obj)
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         # Actual place where we save it to the MEDIA_ROOT (cloud or other)
-
         serializer.save(from_user_id=self.request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
