df799f9d18d0019ffbbfa1ceb0e7f03bc15dae48
asdf134652 <asdf134652@gmail.com>
2022-11-02 12:53:49 +0900

    fix: 🐛 [be-audio] Swagger Multipaser error handling
     #968 Swagger multiparser 사용 시 m:n 필드 문제 해결

--- BE/audios/serializers.py
+++ BE/audios/serializers.py
@@ -1,14 +1,9 @@
 from django.core.validators import FileExtensionValidator
 from rest_framework import serializers
 from .models import Audio
-from django.db import models
-from django.core.validators import int_list_validator
-
-class IntegerListField(serializers.ListField) :
-    child = serializers.IntegerField()
 
 class AudioSerializer(serializers.ModelSerializer):
-    to_user_id = IntegerListField(write_only=True)
+
     audio = serializers.FileField(
         validators=[FileExtensionValidator(allowed_extensions=['m4a', 'mp3'])])
 
--- BE/audios/views.py
+++ BE/audios/views.py
@@ -5,19 +5,22 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
 from rest_framework.parsers import MultiPartParser
-
+from drf_yasg.utils import swagger_auto_schema
 
 class AudioSaveAPIView(CreateAPIView):
     serializer_class = AudioSerializer
     permission_classes = (permissions.IsAuthenticated,)
     parser_classes = [MultiPartParser, ]
 
+    @swagger_auto_schema(request_body=AudioSerializer)
     def post(self, request, *args, **kwargs):
-        file_obj = request.data
-        print(request.data)
-        print(type(request.data['to_user_id']))
-        print(len(request.data['to_user_id']))
-        serializer = AudioSerializer(data=file_obj)
+
+        if len(request.data['to_user_id'])> 1 and ',' in request.data['to_user_id'] :
+            try :
+                request.data.setlist('to_user_id', list(map(int,request.data['to_user_id'].split(','))))
+            except :
+                Response({'잘못된 요청입니다.'},status=status.HTTP_400_BAD_REQUEST)
+        serializer = AudioSerializer(data=request.data)
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
