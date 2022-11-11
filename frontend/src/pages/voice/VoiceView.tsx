59aa5cb61e7fdf7e5a10c63dc427caa1df14f50f
erict4f <erict4f@gmail.com>
2022-11-11 16:03:39 +0900

    feat: 🎸 [FE-voice-view] 음성 메시지 헤더 추가

--- frontend/src/pages/voice/VoiceView.tsx
+++ frontend/src/pages/voice/VoiceView.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 interface highLightProps {
@@ -9,6 +11,7 @@ const SelectViewDiv = styled.div`
   display: flex;
   position: relative;
   height: 2.4em;
+  background-color: #fff;
 `
 const SelectViewItem = styled.div`
   text-align: center;
@@ -38,8 +41,44 @@ const VoiceMessageDiv = styled.div`
   margin: 0.8em;
   background: linear-gradient(45deg, #fec786, #ff787f);
 `
+const HeaderBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+  position: sticky;
+  top: 0px;
+  background-color: #ffcdbe;
+  height: 56px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  z-index: 1;
+`;
+const IconBox = styled.div`
+  margin: auto;
+  line-height: 24px;
+`;
+const HeaderLabel = styled.div`
+  font-size: 1em;
+  font-weight: bold;
+  text-align: center;
+  line-height: 56px;
+`;
+
 const UnviewedVoices = () => {
   
+}
+
+const Header = () => {
+  const navigate = useNavigate();
+  const moveToCreate = () => {
+    navigate("/voice/create");
+  };
+  return (
+    <HeaderBox>
+      <IconBox onClick={moveToCreate}>
+        <FaPlus size={"24"} />
+      </IconBox>
+      <HeaderLabel>{"음성메시지"}</HeaderLabel>
+    </HeaderBox>
+  )
 }
 
 const VoiceView = () => {
@@ -49,6 +88,7 @@ const VoiceView = () => {
 
   return (
     <>
+      <Header />
       <SelectViewDiv>
         <SelectViewItem onClick={() => (setIsLeft(true))}>{`미확인 메시지(${unViewedMessage.length})`}</SelectViewItem>
         <SelectViewItem onClick={() => (setIsLeft(false))}>{`보관함 메시지(${keptMessage.length})`}</SelectViewItem>
