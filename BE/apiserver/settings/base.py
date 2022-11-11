aa714da7e540d867b8f29cbf83bfb7b080236fcb
asdf134652 <asdf134652@gmail.com>
2022-11-11 11:08:32 +0900

    refactor: 💡 remove base db

--- BE/apiserver/settings/base.py
+++ BE/apiserver/settings/base.py
@@ -96,31 +96,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'apiserver.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-
-DATABASES = { 
-	'default': { 
-    	'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'zoa', 
-        'USER': 'root', 
-        'PASSWORD': env('MYSQL_PASSWORD'), 
-        'HOST': 'k7b103.p.ssafy.io', 
-        'PORT': '3306', 
-        'TEST': {
-            'NAME': 'zoa_test',
-        },
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-            'use_unicode': True,
-        },
-     } 
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
