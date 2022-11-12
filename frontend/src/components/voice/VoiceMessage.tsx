8ea934bf9f826ce90ec0a16876e0cabed98ea7c2
박재현 <erict4f@gmail.com>
2022-11-12 14:50:22 +0900

    feat: [FE-voice-view] api 변경 필드 적용

---
+++ frontend/src/components/voice/VoiceMessage.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import styled from "styled-components"
+import { FaPlay, FaPause } from "react-icons/fa"
+
+const VoiceMessageDiv = styled.div`
+  display: flex;
+  align-items: center;
+`
+const VoiceSenderDiv = styled.div`
+  width: 64px;
+  height: 64px;
+  margin-right: 16px;
+`
+const VoiceSenderImg = styled.img`
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+  object-fit: fill;
+`
+const VoiceDiv = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  height: 40px;
+  width: 168px;
+  background-color: #fdaf97;
+  border-radius: 20px;
+`
+const VoiceIconDiv = styled.div`
+  height: 32px;
+  width: 32px;
+  margin: 4px 16px 4px 4px;
+  border-radius: 16px;
+  background-color: #fff;
+  text-align: center;
+  line-height: 36px;
+  color: #FEC786;
+`
+const VoiceTimeDiv = styled.div`
+  color: #fff;
+`
+const VoiceTimeDifference = styled.div`
+  font-size: 0.6rem;
+  bottom: -16px;
+  right: 16px;
+`
+const SenderName = styled.div`
+  width: 64px;
+  margin: 8px 0 16px;
+  text-align: center;
+`
+type VoiceMessageProps = {
+  id: number,
+  image: string,
+  set_name: string,
+  audio: string, // url
+  created_at: string, // date
+  name: string,
+}
+
+const VoiceMessage = ({ id, image, set_name, audio, created_at, name }: VoiceMessageProps) => {
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  const togglePlay = () => {
+    if (isPlaying) {
+      // 재생
+      setIsPlaying(false);
+    } else {
+      // 정지
+      setIsPlaying(true);
+    }
+  }
+
+  return (
+    <>
+      <VoiceMessageDiv>
+        <VoiceSenderDiv>
+          <VoiceSenderImg src={image}/>
+        </VoiceSenderDiv>
+        <VoiceDiv>
+          <VoiceIconDiv onClick={togglePlay}>
+            {isPlaying ? <FaPause /> : <FaPlay /> }
+          </VoiceIconDiv>
+          <VoiceTimeDiv></VoiceTimeDiv>
+          <VoiceTimeDifference></VoiceTimeDifference>
+        </VoiceDiv>
+      </VoiceMessageDiv>
+      <SenderName>{set_name ? set_name : name}</SenderName>
+    </>
+  )
+}
+
+export default VoiceMessage;
---
+++ frontend/src/pages/voice/VoiceView.tsx
@@ -0,0 +1,164 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import styled, { css } from "styled-components";
+import { useAppSelector } from "../../app/hooks";
+import { VoiceMessage } from "../../components/voice"
+
+interface highLightProps {
+  isLeft?: boolean;
+}
+
+const SelectViewDiv = styled.div`
+  display: flex;
+  position: relative;
+  height: 2.4em;
+  background-color: #fff;
+`
+const SelectViewItem = styled.div`
+  text-align: center;
+  line-height: 2.4em;
+  flex: 1;
+`
+const SelectHighlight = styled.div<highLightProps>`
+  width: 50vw;
+  height: 3px;
+  position: absolute;
+  background-color: #FE9B7C;
+  bottom: 0;
+  ${({ isLeft }) => {
+    if (isLeft) {
+      return css`
+        left: 0;
+      `;
+    } else {
+      return css`
+        left: 50%;
+      `
+    }
+  }}
+  transition: left 0.2s;
+`
+const VoiceMessageDiv = styled.div`
+  margin: 0.8em;
+`
+const HeaderBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+  position: sticky;
+  top: 0px;
+  background-color: #ffcdbe;
+  height: 56px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  z-index: 1;
+`;
+const IconBox = styled.div`
+  margin: auto;
+  line-height: 24px;
+`;
+const HeaderLabel = styled.div`
+  font-size: 1em;
+  font-weight: bold;
+  text-align: center;
+  line-height: 56px;
+`;
+
+const UnviewedVoices = () => {
+  
+}
+
+const Header = () => {
+  const navigate = useNavigate();
+  const moveToCreate = () => {
+    navigate("/voice/create");
+  };
+  return (
+    <HeaderBox>
+      <IconBox onClick={moveToCreate}>
+        <FaPlus size={"24"} />
+      </IconBox>
+      <HeaderLabel>{"음성메시지"}</HeaderLabel>
+    </HeaderBox>
+  )
+}
+
+const VoiceView = () => {
+  const [unViewedMessage, setUnViewedMessage] = useState<{
+    id: number, 
+    image: string, 
+    set_name: string, 
+    audio: string, 
+    created_at: string
+    name:string
+  }[]>([]);
+  const [keptMessage, setKeptMessage] = useState<{
+    id: number, 
+    image: string, 
+    set_name: string, 
+    audio: string, 
+    created_at: string
+    name:string
+  }[]>([]);
+  const [isLeft, setIsLeft] = useState<boolean>(true);
+  const accessToken = useAppSelector(state => state.token.access)
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: `${process.env.REACT_APP_BACK_HOST}/audio/?search=0`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((res) => {
+        console.log(res.data);
+        setUnViewedMessage(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  },[])
+
+  return (
+    <>
+      <Header />
+      <SelectViewDiv>
+        <SelectViewItem onClick={() => (setIsLeft(true))}>{`미확인 메시지(${unViewedMessage.length})`}</SelectViewItem>
+        <SelectViewItem onClick={() => (setIsLeft(false))}>{`보관함 메시지(${keptMessage.length})`}</SelectViewItem>
+        <SelectHighlight isLeft={isLeft}/>
+      </SelectViewDiv>
+      <VoiceMessageDiv>
+      {
+        isLeft ? (<>
+          {unViewedMessage.map((message) => (
+            <VoiceMessage 
+              id={message.id}
+              image={message.image}
+              set_name={message.set_name}
+              audio={message.audio}
+              created_at={message.created_at}
+              key={message.id}
+              name={message.name}
+            />
+          ))}
+        </>) : (<>
+          {keptMessage.map((message) => (
+            <VoiceMessage 
+              id={message.id}
+              image={message.image}
+              set_name={message.set_name}
+              audio={message.audio}
+              created_at={message.created_at}
+              key={message.id}
+              name={message.name}
+            />
+          ))}
+        </>)
+      }
+      </VoiceMessageDiv>
+    </>
+  )
+}
+
+export default VoiceView;
