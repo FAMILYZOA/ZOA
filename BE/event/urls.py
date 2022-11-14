09ca49c7150b7d648304af183330cd1aad362089
SSAFY <hohyun323@naver.com>
2022-11-14 17:10:15 +0900

    refactor: 💡 [BE-audio]
    response data 정렬 수정

--- BE/event/urls.py
+++ BE/event/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from event.views import AuthenticationAcceptAPIView, PhoneAuthenticationView
+from event.views import AuthenticationAcceptAPIView, PhoneAuthenticationView, FCMAuthenticationView, 
 
 app_name = 'event'
 
 urlpatterns = [
     path('', PhoneAuthenticationView.as_view(), name="sms_post"),
+
     path('check/', AuthenticationAcceptAPIView.as_view(), name="sms_checker"),
+    
 ]
