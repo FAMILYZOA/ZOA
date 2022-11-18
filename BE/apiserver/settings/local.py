f6f4d777f15a7004f3ee070e0b00deb9bd19ecd8
hohyun <hohyun323@naver.com>
2022-11-18 14:40:32 +0900

    refactor: 💡 [BE-audio]
    request data 형식 변경

--- BE/apiserver/settings/local.py
+++ BE/apiserver/settings/local.py
@@ -32,6 +32,13 @@ DATABASES = {
         },
      } 
 }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 
 
 STATIC_URL = '/static/'
