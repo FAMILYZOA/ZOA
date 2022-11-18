4584694aee0334205134f0c39b741cc4fbd74f40
hohyun <hohyun323@naver.com>
2022-11-18 14:41:12 +0900

    refactor: 💡 [BE-audio]
    audio delete request data 형식 변경

--- BE/apiserver/settings/local.py
+++ BE/apiserver/settings/local.py
@@ -32,13 +32,6 @@ DATABASES = {
         },
      } 
 }
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
 
 
 STATIC_URL = '/static/'
