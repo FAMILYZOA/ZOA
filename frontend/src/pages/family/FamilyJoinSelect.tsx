105028e4f077f42893317635b06e7f0abbb5e6bd
kiki <unhhyyee66@gmail.com>
2022-11-21 00:53:24 +0900

    refactor: 💡 family code join select page header modify

--- frontend/src/pages/family/FamilyJoinSelect.tsx
+++ frontend/src/pages/family/FamilyJoinSelect.tsx
@@ -4,26 +4,40 @@ import styled from "styled-components";
 import { useAppSelector } from "../../app/hooks";
 import logo from "../../assets/white-logo.png";
 import greeting from "../../assets/zoa_greetings.png"
+import {  IoLogOut } from "react-icons/io5";
+import { useAppDispatch } from './../../app/hooks';
+import {
+  setAccessToken,
+  setRefreshToken,
+} from "../../features/token/tokenSlice";
+import { setFcmTokenId } from "../../features/mobile/mobileSlice";
+import axios from "axios";
 
-const HeaderStyle = styled.div`
-  display: flex;
-  flex-direction: column;
+const HeaderBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
   align-items: center;
-  background: #ffffff;
-  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1);
-  height: 64px;
-  width: 100%;
+  justify-content: center;
+  position: sticky;
+  top: 0px;
+  background-color: #ffcdbe;
+  height: 56px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  z-index: 1;
 `;
-const Header = styled.div`
+
+const IconBox = styled.div`
+  margin: auto;
   display: flex;
   justify-content: center;
   align-items: center;
-  position: sticky;
-  top: 0px;
-  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-  background-color: #ffcdbe;
-  height: 56px;
-  box-shadow: rgba(255, 255, 255, 0.3) 0px 1px 4px;
+`;
+
+const ImgBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
   img {
     height: 32px;
   }
@@ -84,38 +98,96 @@ const FamilyParticipateBtn = styled.button`
 const FamilyJoinSelect = () => {
   const navigate = useNavigate();
   const familyId = useAppSelector(state => state.family.id);
-
+  const dispatch = useAppDispatch();
+  const accessToken = useAppSelector((state) => state.token.access);
+  const refreshToken = useAppSelector((state) => state.token.refresh);
+  const fcmTokenId = useAppSelector((state) => state.mobile.fcmTokenId);
   useEffect(() => {
     if (familyId >= 0) {
       navigate("/");
     }
   }, [familyId])
 
+    const logout = () => {
+      if(window.confirm ("로그아웃 하시겠습니까?")){
+        axios({
+          method: "POST",
+          url: `${process.env.REACT_APP_BACK_HOST}/accounts/logout/`,
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+          data: {
+            refresh: `${refreshToken}`,
+          },
+        }).then(() => {
+          dispatch(setAccessToken("")); // 로그아웃 하기
+          dispatch(setRefreshToken(""));
+          localStorage.removeItem("token");
+          axios({
+            method: "DELETE",
+            url: `${process.env.REACT_APP_BACK_HOST}/event/FCM/${fcmTokenId}/`,
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          })
+            .then((res) => {
+              dispatch(setFcmTokenId(""));
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+          navigate("/intro", { replace: true });
+        });
+      } else{
+        
+      }
+    };
+
   return (
     <>
-      <Header>
-        <img src={logo} alt="" />
-      </Header>
+      <HeaderBox>
+        <div></div>
+        <ImgBox>
+          <img src={logo} alt="" />
+        </ImgBox>
+        <IconBox onClick={logout}>
+          <IoLogOut size="24" color="#ff787f"/>
+        </IconBox>
+      </HeaderBox>
       <JoinContents>
-        <div style={{width: "100%"}}>
+        <div style={{ width: "100%" }}>
           <Greetings>
-            <img style={{objectFit: "contain", width: "80%"}} src={greeting} alt="어서오세요!"/>
+            <img
+              style={{ objectFit: "contain", width: "80%" }}
+              src={greeting}
+              alt="어서오세요!"
+            />
           </Greetings>
-          <Guide>가족 생성/참여하여 <br /> 구성원들과 하루를 공유해보세요!</Guide>
+          <Guide>
+            가족 생성/참여하여 <br /> 구성원들과 하루를 공유해보세요!
+          </Guide>
           <BtnBox>
-            <FamilyCreateBtn onClick={() => {navigate("/family/create")}}>
+            <FamilyCreateBtn
+              onClick={() => {
+                navigate("/family/create");
+              }}
+            >
               가족 생성하기
             </FamilyCreateBtn>
           </BtnBox>
           <BtnBox>
-            <FamilyParticipateBtn onClick={() => {navigate("/family/code")}}>
+            <FamilyParticipateBtn
+              onClick={() => {
+                navigate("/family/code");
+              }}
+            >
               참여코드로 참여하기
             </FamilyParticipateBtn>
           </BtnBox>
         </div>
       </JoinContents>
     </>
-  )
+  );
 }
 
 export default FamilyJoinSelect;
